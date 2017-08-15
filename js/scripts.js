1efc0550b5f0f2da27f5c885b414a073589d58e6
Riki Montgomery <mostriki820@gmail.com>
2017-08-14 21:16:43 -0700

    add initial html, css, and js to calculatore.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var add = function(number1, number2) {
+	return number1 + number2;
+}
+var subtract = function(number1, number2) {
+	return number1 - number2;
+}
+var multiply = function(number1, number2) {
+	return number1 * number2;
+}
+var divide = function(number1, number2) {
+	return number1 / number2;
+}
+
+var one_number = parseInt(prompt("Enter a number: "));
+var two_number = parseInt(prompt("Enter an additional number: "));
+
+alert(divide(one_num, two_num));
